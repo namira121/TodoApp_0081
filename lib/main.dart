80236bf3c9f677aa3aacd5dec5e65ed8e68b73c0
Namira Pramadhani <namnam6617@gmail.com>
2025-03-21 03:04:20 +0700

    membuat scaffold untuk page todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp_0081/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoPage(),
     );
   }
 }
--- lib/todo.dart
+++ lib/todo.dart
@@ -10,6 +10,17 @@ class TodoPage extends StatefulWidget {
 class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              
+            ],
+          ),
+          )
+        ),
+    );
   }
 }
